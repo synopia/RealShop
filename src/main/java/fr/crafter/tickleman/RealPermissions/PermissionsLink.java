302f673fcaac5aca61f788ab91af957cae3cefee
U-MATRIX-SEVEN\synopia <synopia@MATRIX-SEVEN.(none)>
2011-08-06 12:25:08 +0200

    made it working with maven

--- src/main/java/fr/crafter/tickleman/RealPermissions/PermissionsLink.java
+++ src/main/java/fr/crafter/tickleman/RealPermissions/PermissionsLink.java
@@ -3,9 +3,6 @@ package fr.crafter.tickleman.RealPermissions;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import com.nijiko.permissions.PermissionHandler;
-import com.nijikokun.bukkit.Permissions.Permissions;
-
 import fr.crafter.tickleman.RealPlugin.RealPlugin;
 
 //########################################################################### class PermissionsLink
@@ -16,28 +13,18 @@ public class PermissionsLink
 
 	private static RealPlugin plugin;
 
-	public static PermissionHandler permissionsHandler;
-
 	//--------------------------------------------------------------------------------- hasPermission
 	public static boolean hasPermission(Player player, String permission)
 	{
-		return permissionsHandler.has(player, plugin.name.toLowerCase() + "." + permission);
+        return player.hasPermission(permission);
 	}
 
 	//------------------------------------------------------------------------------------------ init
 	public static boolean init(RealPlugin plugin)
 	{
-		PermissionsLink.plugin = plugin;
-		Plugin test = plugin.getServer().getPluginManager().getPlugin("Permissions");
-		boolean ok = (test != null);
-		if (ok) {
-			permissionsHandler = ((Permissions)test).getHandler();
-			plugin.log.info("load dependency : Permissions ok", true);
-			initialized = true;
-		} else {
-			plugin.log.severe("load dependency : Permissions could not be linked", true);
-		}
-		return ok;
+        initialized = true;
+    	plugin.log.info("permissions initialized", true);
+        return true;
 	}
 
 }
